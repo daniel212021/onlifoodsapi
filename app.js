52f63d8bef2a8459c22725b391dae45c63e31f77
daniel212021 <72631086+daniel212021@users.noreply.github.com>
2022-01-24 23:31:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -41,23 +41,7 @@ app.get('/', (req, res) => {
   });
 });
 
-const client = new Client({
-  restartOnAuthFail: true,
-  puppeteer: {
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process', // <- this one doesn't works in Windows
-      '--disable-gpu'
-    ],
-  },
-  session: sessionCfg
-});
+const client = new Client({ puppeteer: { headless: false }, clientId: 'whatsapp-api-tutorial-master' });
 
 
 
